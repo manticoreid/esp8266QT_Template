18e69733ca56f30383c3bbf80064c96cae94e087
Budi Prakosa <iyok@deadmediafm.org>
2018-07-01 09:58:53 +0700

    first commit

---
+++ project.ino
@@ -0,0 +1,38 @@
+#include <Arduino.h>
+#include <Ticker.h>
+#include <pgmspace.h>
+#include "lib/AudioOutputI2S.h"
+
+// AudioOutputI2S soundOut;
+
+char ssid[] = "SSIDNAME"; //  your network SSID (name)
+char pass[] = "PASSWORD";    // your network password (use for WPA, or use as key for WEP)
+
+/*
+void ICACHE_RAM_ATTR onTimerISR(){
+
+
+}
+*/
+
+
+void setup()
+{
+/*
+  soundOut.SetRate(44100);
+  soundOut.SetBitsPerSample(16);
+  soundOut.SetChannels(2);
+  soundOut.begin();
+
+  timer1_attachInterrupt(onTimerISR); //Attach our sampling ISR
+  timer1_enable(TIM_DIV16, TIM_EDGE, TIM_SINGLE);
+  timer1_write(2000); //Service at 2mS intervall
+*/
+
+}
+
+
+void loop()
+{
+
+}
